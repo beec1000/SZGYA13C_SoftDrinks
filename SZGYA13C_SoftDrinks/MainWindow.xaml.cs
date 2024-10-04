64d55fffa6bdadecbaa0390d2dd4ee0a03583ddc
beec1000 <beec1000.minecraft@gmail.com>
2024-10-04 14:02:36 +0200

    Add project files.

---
+++ SZGYA13C_SoftDrinks/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SZGYA13C_SoftDrinks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<SoftDrinks> softDrink = new List<SoftDrinks>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            softDrink = SoftDrinks.FromFile(@"..\..\..\src\softDrinks.txt");
+
+            //4.feladat
+
+            Random rnd = new Random();
+
+            var curkosItalok = softDrink.Where(s => s.EdesitoAnyag == "cukor");
+            var randomCukrosItal = rnd.Next(curkosItalok.Count());
+
+            tbl1.Text = $"{softDrink[randomCukrosItal].Nev}";
+
+            //5.feladat
+
+            var gyumolcsMentes = softDrink.Where(s => s.GyumolcsTartalom <= 0)
+                                          .MinBy(s => s.Ar);
+
+            tbl2.Text = $"{gyumolcsMentes.Nev} - {gyumolcsMentes.Ar} Ft/l";
+
+            //6.feladat
+
+            List<string> gyarto = new List<string>();
+
+            string[] gyartok = softDrink.Select(s => s.Nev)
+                                        .Distinct()
+                                        .ToArray();            
+
+            foreach (var g in gyartok)
+            {
+                string s = g.Split(' ')[0];
+
+                gyarto.Add(s);
+            }
+
+            tbl3.Text = $"{gyarto.Distinct().Count()}";
+
+            //7.feladat
+
+            var uditok = softDrink.GroupBy(s => s.Nev)
+                                  .ToList();
+
+
+        }
+    }
+}
---
+++ SZGYA13C_SoftDrinks/SoftDrinks.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SZGYA13C_SoftDrinks
+{
+    internal class SoftDrinks
+    {
+        public string Nev { get; set; }
+        public string EdesitoAnyag { get; set; }
+        public int Ar { get; set; }
+        public string Csomagolas { get; set; }
+        public int GyumolcsTartalom { get; set; } //%
+        public int Darab { get; set; }
+
+        public SoftDrinks (string nev, string edesitoAnyag, int ar, string csomagolas, int gyumolcsTartalom, int darab)
+        {
+            Nev = nev;
+            EdesitoAnyag = edesitoAnyag;
+            Ar = ar;
+            Csomagolas = csomagolas;
+            GyumolcsTartalom = gyumolcsTartalom;
+            Darab = darab;
+        }
+
+        public static List<SoftDrinks> FromFile(string path)
+        {
+            List<SoftDrinks> softDrinks = new List<SoftDrinks>();
+
+            string[] line = File.ReadAllLines(path);
+
+            foreach (var l in line.Skip(1))
+            {
+                string[] s = l.Split(';');
+
+                string Nev = s[0];
+                string EdesitoAnyag = s[1];
+                int Ar = int.Parse(s[2]);
+                string Csomagolas = s[3];
+                int GyumolcsTartalom = int.Parse(s[4]);
+                int Darab = int.Parse(s[5]);
+
+                SoftDrinks sofDrink = new (Nev, EdesitoAnyag, Ar, Csomagolas, GyumolcsTartalom, Darab);
+
+                softDrinks.Add(sofDrink);
+            }
+            return softDrinks;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev}, {EdesitoAnyag}, {Ar}, {Csomagolas}, {GyumolcsTartalom}, {Darab}";
+        }
+    }
+}
